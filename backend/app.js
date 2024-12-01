72279015a5f363edee40d4b0e9dbe1e89cc3da02
kmalviya986@gmail.com <your-email@example.com>
2024-12-01 22:34:42 +0530

    profile and logout is created

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,14 @@ const cors = require("cors");
 const app = express();
 const connectedToDbb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
+const cookieParser = require('cookie-parser');
 
 connectedToDbb();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("hello krishnkant")
